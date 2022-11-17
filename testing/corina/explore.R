a141b5b4750580727c2774db2d09e9fa9e9b23a5
corinaramont <corinaramont@tamu.edu>
2022-11-17 14:35:05 -0600

    create boxplot & histogram of radius mean

--- testing/corina/explore.R
+++ testing/corina/explore.R
@@ -1,2 +1,10 @@
 setwd("~/stat406-project/datasets")
 data = read.csv("data.csv")
+
+
+param_names = colnames(data)
+#unique ids
+
+#radius
+boxplot(data$radius_mean, main="Radius Mean")
+hist(data$radius_mean, main = "Distribution of Radius Mean")
