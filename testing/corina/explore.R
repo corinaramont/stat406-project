6d9c02d33dfb74978bf00504fed86fa3f1efcf01
corinaramont <corinaramont@tamu.edu>
2022-11-17 21:15:49 -0600

    checked dist of texture mean & radius SE, radius mean also not normally dist

--- testing/corina/explore.R
+++ testing/corina/explore.R
@@ -8,3 +8,13 @@ param_names = colnames(data)
 #radius
 boxplot(data$radius_mean, main="Radius Mean")
 hist(data$radius_mean, main = "Distribution of Radius Mean")
+shapiro.test(data$radius_mean) #not normally dist
+
+boxplot(data$radius_se, main="Radius SE")
+hist(data$radius_se, main = "Distribution of Radius SE")
+
+#texture
+boxplot(data$texture_mean, main="Texture Mean")
+hist(data$texture_mean, main = "Distribution of Texture Mean")
+
+#
