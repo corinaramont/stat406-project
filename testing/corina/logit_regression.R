f4e01367b2c12c0aa44781d4571096312f294e19
corinaramont <corinaramont@tamu.edu>
2022-11-29 10:12:19 -0600

    recode diagnoses to 0,1

--- testing/corina/logit_regression.R
+++ testing/corina/logit_regression.R
@@ -15,13 +15,22 @@ data = read.csv("data.csv")
 #  smoothness_worst + compactness_worst + concavity_worst + 
 #  concave.points_worst + symmetry_worst + fractal_dimension_worst
 
+data1 = data
+
+for(i in 1:nrow(data1)){
+  if(data1$diagnosis[i] == "M"){
+    data1$diagnosis[i] = 1
+  }else{
+    data1$diagnosis[i] = 0
+  }
+}
 
 #fitting model with their means
 model = glm(as.factor(diagnosis) ~ radius_mean + texture_mean + 
               perimeter_mean + area_mean + smoothness_mean + 
               compactness_mean + concavity_mean + concave.points_mean +
               symmetry_mean + fractal_dimension_mean,
-            data = data, family=binomial)
+            data = data1, family=binomial)
 summary(model)
 
 step(model, direction = "backward")
@@ -30,18 +39,18 @@ step(model, direction = "backward")
 model_w = glm(as.factor(diagnosis) ~ radius_worst + texture_worst + perimeter_worst + area_worst + 
                 smoothness_worst + compactness_worst + concavity_worst + 
                 concave.points_worst + symmetry_worst + fractal_dimension_worst,
-              data = data, family=binomial)
+              data = data1, family=binomial)
 summary(model_w)
 step(model_w, direction = "backward")
 
 #fitting new models with aic suggestions
 new_model= glm(as.factor(diagnosis) ~ radius_mean + texture_mean + area_mean 
                + smoothness_mean + concave.points_mean + symmetry_mean, 
-               data = data, family = binomial)
+               data = data1, family = binomial)
 new_model_w = glm(as.factor(diagnosis) ~ texture_worst + 
                     area_worst + smoothness_worst + compactness_worst + 
                     concavity_worst + concave.points_worst + symmetry_worst,
-                  data = data, family = binomial)
+                  data = data1, family = binomial)
 
 
 #compare log likelihood values
